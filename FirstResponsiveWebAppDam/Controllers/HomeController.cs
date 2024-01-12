90e4e414398661661ed06f716084546ecf1937fe
Eric <127010882+Dcrievn@users.noreply.github.com>
2024-01-12 17:47:48 -0600

    FirstResponsiveWebApp Commit

---
+++ FirstResponsiveWebAppDam/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FirstResponsiveWebAppDam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppDam.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.AgeThisYear();
+            }
+            else
+            {
+                ViewBag.Age = 0;
+            }
+            return View(model);
+        }
+    }
+}
+
---
+++ FirstResponsiveWebAppDam/Models/AgeModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppDam.Models
+{
+    public class AgeModel
+    {
+
+        public DateTime today = DateTime.Now;
+
+        [Required(ErrorMessage = "This field can not be empty")]
+        [StringLength(20, ErrorMessage = "Name length can't be more than 20.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a birth year")]
+        [Range(1906, 2023, ErrorMessage = "Birth year must be between 1906 and 2023.")]
+        /* range starts at 1906 as it is the birth year of the current oldest living person
+         and ends at 2023 as age cannot be whole number if born in current year 2024 */
+        public int? BirthYear { get; set; }
+        public int? AgeThisYear()
+        {
+            int? age = today.Year - BirthYear;
+            return age;
+        }
+    }
+}
