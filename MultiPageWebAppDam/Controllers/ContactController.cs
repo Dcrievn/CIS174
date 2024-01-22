2d5e68a5db5447e09949024e7937b5eb68d29376
Eric <127010882+Dcrievn@users.noreply.github.com>
2024-01-21 18:51:21 -0600

    MultiWebPageApp Commit

---
+++ MultiPageWebAppDam/Controllers/ContactController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiPageWebAppDam.Models;
+
+namespace MultiPageWebAppDam.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactContext context { get; set; }
+        public ContactController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+        
+        // Allows the usage of adding new contacts
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new ContactInfo());
+        }
+
+        // Allows editing of contacts
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+        [HttpPost]
+        public IActionResult Edit(ContactInfo contact)
+        {
+            if (ModelState.IsValid)
+            {
+                if (contact.ContactID == 0)
+                    context.Contacts.Add(contact);
+                else
+                    context.Contacts.Update(contact);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (contact.ContactID == 0) ? "Add" : "Edit";
+                return View(contact);
+            }
+        }
+
+        // allows deleting contacts
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var contact = context.Contacts.Find(id);
+            return View(contact);
+        }
+        [HttpPost]
+        public IActionResult Delete(ContactInfo contact)
+        {
+            context.Contacts.Remove(contact);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ MultiPageWebAppDam/Models/ContactInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MultiPageWebAppDam.Models
+{
+    public class ContactInfo
+    {
+        [Key]
+        public int ContactID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        // set as string to allow dashes between numbers if user chooses to do so
+        [Required(ErrorMessage = "Please enter a phone number.")]
+        [DataType(DataType.PhoneNumber)]
+        [Phone]
+        public string PhoneNumber { get; set; } 
+
+        [Required(ErrorMessage = "Please enter an address.")]
+        public string Address { get; set; }
+
+        // note is set to nullable as I believe it isn't an important aspect when making new contacts
+        public string? Note { get; set; }
+        public string? Slug => Name?.Replace(' ', '-').ToLower();
+    }
+}
