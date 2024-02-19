ffbdfcf02d75ccbc44411d488e8f77486df3dc96
Eric <127010882+Dcrievn@users.noreply.github.com>
2024-02-19 01:02:46 -0600

    RazorWebsite Commit

---
+++ RazorWebsite/Controllers/AssignmentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorWebsite.Models;
+
+namespace RazorWebsite.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        [Route("[controller]/[action]/{accessLevel}")]
+        public ActionResult Index(int accessLevel)
+        {
+            var students = new List<Student>
+            {
+                new Student { FirstName = "Eric", LastName = "Dam", Grade = 100 },
+                new Student { FirstName = "Joe", LastName = "Smith", Grade = 30 },
+                new Student { FirstName = "Bob", LastName = "Johnson", Grade = 78 },
+            };
+            var viewModel = new AssignmentViewModel
+            {
+                Students = students,
+                AccessLevel = accessLevel
+            };
+
+            return View("Index", viewModel);
+        }
+    }
+}
